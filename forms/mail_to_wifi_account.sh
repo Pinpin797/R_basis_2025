1abd7523c8bbd6f9df77af4a694eb11be973da17
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2021-09-15 11:07:08 +0200

    add mail_to_wifi_account.sh: to request wifi account at the ENS

---
+++ forms/mail_to_wifi_account.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+# Convert list of mail to the format : NOM;Prenom 
+# for the page https://instella.ens-lyon.fr/stella/intra/compteInvite.html -> Ajouter une liste de comptes
+
+cat *.csv | grep -v "@ens-lyon.fr" | sed -e 's/\(.*\)\.\(.*\)@.*/\2 \1/g' | awk 'BEGIN { OFS="" }{print toupper($1),";",  $2}'
