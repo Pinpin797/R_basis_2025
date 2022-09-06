63d04813b7a29a4b5a0cc25ff13f5bf1b3ea864d
Carine Rey <carine.rey@ens-lyon.fr>
2022-09-06 11:45:15 +0200

    create a dedicated docker image

---
+++ src/create_docker_to_deploy_the_course.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail +o nounset
+
+
+TAG="v2022"
+
+IMAGE_NAME="r_for_beginners"
+DOCKERFILE_DIR="."
+
+REPO=carinerey/$IMAGE_NAME
+
+echo "## Build docker: $REPO:$TAG ##"
+docker build -t $REPO:$TAG $DOCKERFILE_DIR
+echo "## Build docker: $REPO ##"
+docker build -t $REPO $DOCKERFILE_DIR
+
+if [[ $1 == "push_yes" ]]
+then
+    echo "## Push docker ##"
+    docker push $REPO:$TAG
+    docker push $REPO
+fi
