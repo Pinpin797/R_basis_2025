df6b231098b511c8dddfa8ad074d9995c6ed58d5
Carine Rey <carine.rey@ens-lyon.fr>
2022-09-26 17:23:01 +0200

    update usage

--- src/create_users_from_user_pwd_list.sh
+++ src/create_users_from_user_pwd_list.sh
@@ -1,8 +1,9 @@
 #! /usr/bin/bash
 
 # USAGE
-# upload create_users_from_user_pwd_list.sh and user_pwd_list.tsv
-# bash create_users_from_user_pwd_list.sh user_pwd_list.tsv
+# wget https://gitbio.ens-lyon.fr/can/R_basis/-/raw/master/src/create_users_from_user_pwd_list.sh
+# upload X_user_pwd_list.tsv from your computer to the rstudio server
+# sudo bash create_users_from_user_pwd_list.sh X_user_pwd_list.tsv
 
 USER_PASSWORD_FILENAME=$@
 while IFS=$'\t' read -r GROUPE NAME SURNAME MAIL LOGIN_CBP PASSWD_CBP LABO R_USERNAME R_PASSWD ; do
