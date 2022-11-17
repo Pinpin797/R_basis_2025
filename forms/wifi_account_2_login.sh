5001ae70a510ecb5b3a591a5a30e91ccb714ff42
Carine Rey <carine.rey@ens-lyon.fr>
2022-11-17 12:05:46 +0100

    add getting password

--- forms/wifi_account_2_login.sh
+++ forms/wifi_account_2_login.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/bash
 
 # list wifi loing from pdf
-
+echo "login"
 pdfgrep "Votre nom de connexion ou login" *.pdf | sed -E 's/.*:.*(z.*)/\1/g'
+echo
+echo "mot de passe"
+pdfgrep "Votre mot de passe initial" FicheLogin*.pdf | sed -E 's/.*:.*([a-Z0-9]{8})$/\1/g'
