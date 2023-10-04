4ff60587119f19753415c84cfb6f10c57a19177f
Ghislain Durif <gd.dev@libertymail.net>
2023-10-04 16:59:59 +0200

    fix doc in comment

--- src/create_users_from_user_list_csv.sh
+++ src/create_users_from_user_list_csv.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/bash
 
 # USAGE
-# wget https://gitbio.ens-lyon.fr/can/R_basis/-/raw/master/src/create_users_from_user_list_list_csv.sh
+# wget https://gitbio.ens-lyon.fr/can/R_basis/-/raw/main/src/create_users_from_user_list_csv.sh
 # upload r_user_list_<day_number>_<day>.csv from your computer to the rstudio server
-# sudo bash create_users_from_user_list_list_csv.sh r_user_list_<day_number>_<day>.csv
+# sudo bash create_users_from_user_list_csv.sh r_user_list_<day_number>_<day>.csv
 
 USER_PASSWORD_FILENAME=$@
 while IFS=';' read -r NAME SURNAME EMAIL LAB COMMENT STATUS USERNAME PASSWD ; do
